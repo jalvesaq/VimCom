a66a04ac8340b572224fab1329d1a4ff69111887
Jakson Aquino <jalvesaq@gmail.com>
2012-04-16 00:15:34 -0300

    Replace "Sweave" with "interlace".

---
+++ R/viminterlace.R
@@ -0,0 +1,18 @@
+
+vim.interlace <- function(rnowebfile, latexcmd = "pdflatex", bibtex = FALSE,
+                       knit = FALSE, ...)
+{
+    if(knit){
+        Sres <- knit(rnowebfile, ...)
+    } else {
+        Sres <- Sweave(rnowebfile, ...)
+    }
+    if(exists('Sres')){
+        system(paste(latexcmd, Sres))
+        if(bibtex){
+            system(paste("bibtex", sub("\\.tex$", ".aux", Sres)))
+            system(paste(latexcmd, Sres))
+            system(paste(latexcmd, Sres))
+        }
+    }
+}
