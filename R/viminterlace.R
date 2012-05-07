b32af757f63eff249809849f57385500f55da5de
Jakson Aquino <jalvesaq@gmail.com>
2012-05-07 17:16:00 -0300

    Open PDF document generated from rnoweb file.

--- R/viminterlace.R
+++ R/viminterlace.R
@@ -1,6 +1,6 @@
 
 vim.interlace <- function(rnowebfile, latexcmd = "pdflatex", bibtex = FALSE,
-                       knit = FALSE, ...)
+                          knit = FALSE, view = TRUE, ...)
 {
     if(knit){
         Sres <- knit(rnowebfile, ...)
@@ -13,6 +13,17 @@ vim.interlace <- function(rnowebfile, latexcmd = "pdflatex", bibtex = FALSE,
             system(paste("bibtex", sub("\\.tex$", ".aux", Sres)))
             system(paste(latexcmd, Sres))
             system(paste(latexcmd, Sres))
+        }
+        if(view){
+            # Copyed from RShowDoc()
+            pdfviewer <- getOption("pdfviewer")
+            path <- sub("\\.tex$", ".pdf", Sres)
+            if (!identical(pdfviewer, "false")) {
+                if (.Platform$OS.type == "windows" && identical(pdfviewer, file.path(R.home("bin"), "open.exe")))
+                    shell.exec(path)
+                else 
+                    system2(pdfviewer, shQuote(path), wait = FALSE)
+            }
         }
     }
 }
