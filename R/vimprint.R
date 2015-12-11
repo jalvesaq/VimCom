55665f7063fbac866b4532a6ee4de39471273a2a
Jakson Alves de Aquino <jalvesaq@gmail.com>
2015-12-11 20:58:51 -0300

    Adjust code to RGetClassFor replacement

--- R/vimprint.R
+++ R/vimprint.R
@@ -1,21 +1,14 @@
 
-vim.print <- function(object, classfor)
+vim.print <- function(object, objclass)
 {
     if(!exists(object))
         stop("object '", object, "' not found")
-    if(!missing(classfor) & length(grep(object, names(.knownS3Generics))) > 0){
-        curwarn <- getOption("warn")
-        options(warn = -1)
-        try(classfor <- classfor, silent = TRUE)  # classfor may be a function
-        try(.theclass <- class(classfor), silent = TRUE)
-        options(warn = curwarn)
-        if(exists(".theclass")){
-            for(cls in .theclass){
-                if(exists(paste(object, ".", .theclass, sep = ""))){
-                    .newobj <- get(paste(object, ".", .theclass, sep = ""))
-                    warning("Printing ", object, ".", .theclass, "\n", sep = "")
-                    break
-                }
+    if(!missing(objclass) & length(grep(object, names(.knownS3Generics))) > 0){
+        for(cls in objclass){
+            if(exists(paste(object, ".", objclass, sep = ""))){
+                .newobj <- get(paste(object, ".", objclass, sep = ""))
+                warning("Printing ", object, ".", objclass, "\n", sep = "")
+                break
             }
         }
     }
